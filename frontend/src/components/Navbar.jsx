6f83f41319c3b3c37ff8730b22d2123a4955549f
AnuragKuumar <anuragworld12@gmail.com>
2026-03-08 18:10:14 +0530

    Added call button in header and fixed text visibility on hero section

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -32,6 +32,9 @@ const Navbar = () => {
             <div className="nav-links">
               <Link to="/" className="nav-link">Home</Link>
               <Link to="/search" className="nav-link">Search</Link>
+              <a href="tel:+919202695501" className="call-now-btn">
+                📞 Call: 9202695501
+              </a>
             </div>
           </div>
         </div>
